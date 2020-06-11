a09d2085eafd3abb12080e6068539243ac6e2edd
mutazhanafi <mutazhanafi@gmail.com>
2020-06-11 14:44:59 -0700

    login with email or phone

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,11 +12,11 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="mobile" class="col-md-4 col-form-label text-md-right">{{ __('MOBILE') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email or Mobile') }}</label>
 
                             <div class="col-md-6">
-                                <input id="mobile" type="emtextail" class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" >
-                                @error('mobile')
+                                <input id="identify" type="text" class="form-control @error('identify') is-invalid @enderror" name="identify" value="{{ old('identify') }}" >
+                                @error('identify')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
