f5fcddd9d954fd069c571330aa115f3c82a09d88
mutazhanafi <mutazhanafi@gmail.com>
2020-06-16 15:56:33 -0700

    sendd email all user

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\exipiration::class,
     ];
 
     /**
@@ -24,7 +24,15 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command('inspire')
+        //          ->hourly();
+
+
+        $schedule->command('user:expire')
+            ->everyMinute();
+
+        $schedule->command('notify:email')
+            ->everyMinute();
     }
 
     /**
